cf180a5b637825c90c5ed1f13e97b59c3a26c6b9
paulodev <paulomoreirarosado@hotmail.com>
2025-05-05 13:36:33 -0300

    Fix(Correção de acordo com o anunciado do repositorio da atividade)

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Função para mover a Torre
+// Função para mover a Torre;
 void move_tower(int moviments) {
   if (moviments == 0)
     return;
@@ -8,46 +8,43 @@ void move_tower(int moviments) {
   move_tower(moviments - 1);
 }
 
-// Função para mover o Bispo (recursão e loops aninhados)
+// Função para mover o Bispo (recursão e loops aninhados);
 void move_bishop(int movimentos) {
   if (movimentos == 0)
     return;
 
-  // Movimento vertical (cima)
+  // Loop aninhado para movimentodiagonal (Cima e Direita);
   for (int i = 0; i < 1; i++) {
     printf("Cima\n");
-  }
-
-  // Movimento horizontal (direita)
-  for (int j = 0; j < 1; j++) {
-    printf("Direita\n");
+    for (int j = 0; j < 1; j++) {
+      printf("Direita\n");
+    }
   }
 
   move_bishop(movimentos - 1);
 }
 
-// Função para mover a Rainha
+// Função para mover a Rainha;
 void move_queen(int movimentos) {
   if (movimentos == 0)
     return;
 
-  // Movimento para a esquerda
-  for (int i = 0; i < 1; i++) {
-    printf("Esquerda\n");
-  }
+  // Movimento para a esquerda;
+  printf("Esquerda\n");
 
   move_queen(movimentos - 1);
 }
 
-// Função para mover o Cavalo (loops aninhados)
+// Função para mover o Cavalo loops ainhados e múltiplas condições;
 void move_horse(int vertical, int horizontal) {
+  // Loop aninhado para mover o nCavali em "L"
   for (int i = 0; i < vertical; i++) {
     for (int j = 0; j < horizontal; j++) {
       if (i == 0) {
         printf("Cima\n");
       }
       if (j == 0) {
-        printf("Esquerda\n");
+        printf("Direita\n");
       }
     }
   }
